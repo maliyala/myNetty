464321439ea9e2fe39edaa1693ec4d4641a08b6a
malirui <malirui@meituan.com>
2019-01-30 10:02:50 +0800

    拆包粘包理论和解决方案分支Initial Commit

--- src/main/java/com/malirui/ep/sonar/netty/protocol/command/Command.java
+++
@@ -1,6 +0,0 @@
-package com.malirui.ep.sonar.netty.protocol.command;
-
-public interface Command {
-
-    Byte LOGIN_REQUEST = 1;
-}
--- src/main/java/com/malirui/ep/sonar/netty/protocol/serializer/SerializerAlgorithm.java
+++
@@ -1,8 +0,0 @@
-package com.malirui.ep.sonar.netty.protocol.serializer;
-
-public interface SerializerAlgorithm {
-    /**
-     * json 序列化标志
-     */
-    byte JSON = 1;
-}
